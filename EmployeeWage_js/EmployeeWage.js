647b8451bdd554b094fe5e3b3ea95d8d61a17c70
“preetimca2016” <preetimca2016@gmail.com>
2022-10-07 23:35:14 +0530

    Ability to store the Day

--- EmployeeWage_js/EmployeeWage.js
+++ EmployeeWage_js/EmployeeWage.js
@@ -307,5 +307,41 @@
   }
 
 
+{
+    // UC10 : Ability to store the Day, Hours Worked and Wage Earned in a single object.
+    const MAX_HRS_IN_MONTH = 160;
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyHrsAndWageArr = new Array();
+    while (
+      totalEmpHrs <= MAX_HRS_IN_MONTH &&
+      totalWorkingDays < NUM_OF_WORKING_DAYS
+    ) {
+      totalWorkingDays++;
+      let empCheck = Math.floor(Math.random() * 10) % 3;
+      let empHrs = getWorkingHours(empCheck);
+      totalEmpHrs += empHrs;
+      empDailyHrsAndWageArr.push({
+        dayNum: totalWorkingDays,
+        dailyHours: empHrs,
+        dailyWage: calcDailyWage(empHrs),
+        toString() {
+          return (
+            "\nDay" +
+            this.dayNum +
+            " => Working Hours is " +
+            this.dailyHours +
+            " And Wage Earned = " +
+            this.dailyWage
+          );
+        },
+      });
+    }
+    console.log(
+      "\nUC10 Showing Daily Hours Worked and Wage Earned: " +
+        empDailyHrsAndWageArr
+    );
+  
 
 

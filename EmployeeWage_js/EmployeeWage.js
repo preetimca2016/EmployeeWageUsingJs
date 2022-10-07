b5299592961f7c2debf596e811224640bf3683c8
“preetimca2016” <preetimca2016@gmail.com>
2022-10-07 23:21:18 +0530

    Added:Storing DailyWage in a Array

--- EmployeeWage_js/EmployeeWage.js
+++ EmployeeWage_js/EmployeeWage.js
@@ -95,5 +95,36 @@
         " EmpWage: " +
         empWage
     );
-  }
+  }//UC6---
+  {
+    function calcDailyWage(empHrs) {
+      return empHrs * WAGE_PER_HOUR;
+    }
+    const MAX_HRS_IN_MONTH = 160;
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyWageArr = new Array();
+  
+    while (
+      totalEmpHrs <= MAX_HRS_IN_MONTH &&
+      totalWorkingDays < NUM_OF_WORKING_DAYS
+    ) {
+      totalWorkingDays++;
+      let empCheck = Math.floor(Math.random() * 10) % 3;
+      let empHrs = getWorkingHours(empCheck);
+      totalEmpHrs += empHrs;
+      empDailyWageArr.push(calcDailyWage(empHrs));
+    }
+    let empWage = calcDailyWage(totalEmpHrs);
+    console.log("Employee wage array: " + empDailyWageArr.join(" "));
+    console.log(
+      "UC6--- Total Days: " +
+        totalWorkingDays +
+        " Total Hrs: " +
+        totalEmpHrs +
+        " Emp Wage: " +
+        empWage
+    );
+
 

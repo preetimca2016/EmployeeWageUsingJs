ea55ff2325845d5786ba37e19a678e106a9e6652
“preetimca2016” <preetimca2016@gmail.com>
2022-10-07 23:16:05 +0530

    Added:UsingForLoop

--- EmployeeWage_js/EmployeeWage.js
+++ EmployeeWage_js/EmployeeWage.js
@@ -54,4 +54,21 @@
     let empHrs = getWorkingHours(empCheck);
     let empWage = empHrs * WAGE_PER_HOUR;
     console.log("UC3--- Hour:" + empHrs + " EmpWage:" + empWage);
-  } 
+  }//uc4 for loop
+  {
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+      let empCheck = Math.floor(Math.random() * 10) % 3;
+      totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log(
+      "UC4--- Number of Days: " +
+        NUM_OF_WORKING_DAYS +
+        " Total Hrs:" +
+        totalEmpHrs +
+        " Emp Wage:" +
+        empWage
+    );
+  }
